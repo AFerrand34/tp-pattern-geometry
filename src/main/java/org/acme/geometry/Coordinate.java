6b3380a1a81884774168870577b7612c0418a23e
AFerrand34 <alexandre.ferrand@ensg.eu>
2021-01-04 15:43:13 +0100

    test

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,24 @@
 package org.acme.geometry;
 
 public class Coordinate {
+	private double x = 0.0;
+	private double y = 0.0;
+	
+	public Coordinate() {
+		
+	}
+	
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
 
 }
